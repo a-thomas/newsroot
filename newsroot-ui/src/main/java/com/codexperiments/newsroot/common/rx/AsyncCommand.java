84945c18e7451a4e606bd2d0582cf0becba048b6
ratamovic <ratamovic@gmail.com>
2013-10-22 09:38:56 +0200

    Yes!

--- newsroot-ui/src/main/java/com/codexperiments/newsroot/common/rx/AsyncCommand.java
+++ newsroot-ui/src/main/java/com/codexperiments/newsroot/common/rx/AsyncCommand.java
@@ -13,9 +13,10 @@ import com.codexperiments.newsroot.ui.activity.AndroidScheduler;
 
 public class AsyncCommand<TParam, TResult> extends Observable<TResult> implements Observer<TParam> {
     // ScheduledSubject<Exception> exceptions;
-    private PublishSubject<TParam> mCommand = PublishSubject.create();
-    private PublishSubject<TParam> mBeforeExecute = PublishSubject.create(); // TODO Lazy
-    private PublishSubject<TResult> mAfterExecute = PublishSubject.create(); // TODO Lazy
+    private PublishSubject<TParam> mCommand;
+    // private PublishSubject<TParam> mBeforeExecute = PublishSubject.create(); // TODO Lazy
+    // private PublishSubject<TResult> mAfterExecute = PublishSubject.create(); // TODO Lazy
+    private PublishSubject<Boolean> mIsRunning;// TODO Lazy
     private Observable<TResult> mResult;
     private Scheduler mScheduler = AndroidScheduler.threadForUI();
 
@@ -29,47 +30,81 @@ public class AsyncCommand<TParam, TResult> extends Observable<TResult> implement
     // return new AsyncCommand<TParam, TResult>(pAsyncCommand, pAfterCommand);
     // }
 
-    protected AsyncCommand(Func1<TParam, Observable<TResult>> pAsyncCommand, Func1<TResult, TResult> pAfterCommand) {
+    protected AsyncCommand(final Func1<TParam, Observable<TResult>> pAsyncCommand, Func1<TResult, TResult> pAfterCommand) {
         super(null);
         mCommand = PublishSubject.create();
-        Observable<Observable<TResult>> execution = mCommand// .map(notifyExec(mBeforeExecute)) // TODO Ensure on UI Thread
-        .map(pAsyncCommand);
+        mIsRunning = PublishSubject.create();
+
+        // Observable<Observable<TResult>> execution = runningIndicator(pAsyncCommand);
+        // mCommand// .map(notifyExec(mBeforeExecute)) // TODO Ensure on UI Thread
+        // .map(new Func1<TParam, Observable<TResult>>() {
+        // public Observable<TResult> call(TParam pT1) {
+        // return startRunning(pAsyncCommand.call(pT1)); // TODO Catch
+        // }
+        // });
         /*
          * .map(notifyExec2(mAfterExecute)) .finallyDo(exec(mAfterExecute))
          */; // TODO Publish?
-        mResult = connect(Observable.merge(execution).observeOn(mScheduler).publish()); // .map(pAfterCommand);
+        mResult = connect(Observable.merge(indicateIfRunning(pAsyncCommand)).observeOn(mScheduler).publish()); // .map(pAfterCommand);
     }
 
-    public Observable<TResult> connect(ConnectableObservable<TResult> pObservable) {
+    // protected Observable<TResult> startRunning(Observable<TResult> pAsyncObservable) {
+    // return pAsyncObservable.finallyDo(new Action0() {
+    // public void call() {
+    // mRunning.onNext(Boolean.FALSE);
+    // }
+    // });
+    // }
+
+    protected Observable<Observable<TResult>> indicateIfRunning(final Func1<TParam, Observable<TResult>> pAsyncCommand) {
+        // .map(notifyExec(mBeforeExecute)) // TODO Ensure on UI Thread
+        return mCommand.map(new Func1<TParam, Observable<TResult>>() {
+            public Observable<TResult> call(TParam ppAsyncObservable) {
+                return pAsyncCommand.call(ppAsyncObservable) //
+                                    .finallyDo(new Action0() {
+                                        public void call() {
+                                            mIsRunning.onNext(Boolean.FALSE);
+                                        }
+                                    }); // TODO Catch
+            }
+        }).map(new Func1<Observable<TResult>, Observable<TResult>>() {
+            public Observable<TResult> call(Observable<TResult> pAsyncObservable) {
+                mIsRunning.onNext(Boolean.TRUE);
+                return pAsyncObservable;
+            }
+        });
+    }
+
+    protected Observable<TResult> connect(ConnectableObservable<TResult> pObservable) {
         pObservable.connect();
         return pObservable;
     }
 
-    public Func1<TParam, TParam> notifyExec(final PublishSubject<TParam> pPublisher) {
-        return new Func1<TParam, TParam>() {
-            public TParam call(TParam pParam) {
-                pPublisher.onNext(pParam);
-                return pParam;
-            }
-        };
-    }
-
-    public Func1<TResult, TResult> notifyExec2(final PublishSubject<TResult> pPublisher) {
-        return new Func1<TResult, TResult>() {
-            public TResult call(TResult pResult) {
-                pPublisher.onNext(pResult);
-                return pResult;
-            }
-        };
-    }
-
-    public Action0 exec(final PublishSubject<TParam> pPublisher) {
-        return new Action0() {
-            public void call() {
-                pPublisher.onNext(null);
-            }
-        };
-    }
+    // public Func1<TParam, TParam> notifyExec(final PublishSubject<TParam> pPublisher) {
+    // return new Func1<TParam, TParam>() {
+    // public TParam call(TParam pParam) {
+    // pPublisher.onNext(pParam);
+    // return pParam;
+    // }
+    // };
+    // }
+    //
+    // public Func1<TResult, TResult> notifyExec2(final PublishSubject<TResult> pPublisher) {
+    // return new Func1<TResult, TResult>() {
+    // public TResult call(TResult pResult) {
+    // pPublisher.onNext(pResult);
+    // return pResult;
+    // }
+    // };
+    // }
+    //
+    // public Action0 exec(final PublishSubject<TParam> pPublisher) {
+    // return new Action0() {
+    // public void call() {
+    // pPublisher.onNext(null);
+    // }
+    // };
+    // }
 
     // protected Observable<TResult> merge(Observable<Observable<TResult>> pCommand) {
     // return Observable.create(new OnSubscribeFunc<TResult>() {
@@ -104,6 +139,10 @@ public class AsyncCommand<TParam, TResult> extends Observable<TResult> implement
     @Override
     public Subscription subscribe(Observer<? super TResult> pObserver) {
         return mResult.subscribe(pObserver);
+    }
+
+    public Observable<Boolean> isRunning() {
+        return mIsRunning;
     }
 
     @Override
